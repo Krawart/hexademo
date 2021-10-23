95b522ce0250f23a02303d640e5d71c8897aacd7
Filip Krawiec <filip.krawiec@3pillarglobal.com>
2021-10-23 17:56:14 +0200

    Initial commit

---
+++ src/test/java/com/krawart/hexademo/HexaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krawart.hexademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
